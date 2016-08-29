2241ad6340933dc12f2a413ffdb1402be9005cc5
superpanic <superpanic@gmail.com>
2016-08-29 20:52:19 +0200

    Initial Commit

---
+++ CapsLockLight/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CapsLockLight
+//
+//  Created by Fredrik Josefsson on 29/8/16.
+//  Copyright © 2016 Fredrik Josefsson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
