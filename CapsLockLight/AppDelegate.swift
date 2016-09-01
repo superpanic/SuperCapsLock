b8f3cb33890214ed891c60fa4ce2112cef9c92ec
superpanic <superpanic@gmail.com>
2016-09-01 14:44:43 +0200

    First commit

--- CapsLockLight/AppDelegate.swift
+++ CapsLockLight/AppDelegate.swift
@@ -1,27 +1,126 @@
 //
 //  AppDelegate.swift
-//  CapsLockLight
+//  SuperCapsLock
 //
 //  Created by Fredrik Josefsson on 29/8/16.
 //  Copyright © 2016 Fredrik Josefsson. All rights reserved.
 //
 
 import Cocoa
+import Carbon
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 	@IBOutlet weak var window: NSWindow!
-
-
+	
+	// the status menu item
+	let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
+	
+	// the menu
+	let menu = NSMenu()
+	
+	// tags used for the menu items
+	enum menuTags: Int {
+		case SUPERCAPSLOCK = 1
+		case ABOUT
+		case QUIT
+	}
+	
+	// the menu icon
+	var button: NSStatusBarButton!
+	
+	// keyboard light controller
+	let keyboard_light = KeyboardBacklight()
+	
 	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		// Insert code here to initialize your application
+		
+		// user needs to set accessability privileges for this app
+		acquirePrivileges()
+
+		// setup the menu icon
+		button = statusItem.button
+		button.image = NSImage(named: "StatusItemIconBlk")
+		
+		menu.addItem( NSMenuItem(title: "Caps lock off", action: nil, keyEquivalent: ""))
+		menu.itemAtIndex(menu.numberOfItems-1)?.tag = menuTags.SUPERCAPSLOCK.rawValue
+
+		menu.addItem( NSMenuItem(title: "About SuperCapsLock", action: #selector(AppDelegate.openAboutWindow(_:)), keyEquivalent: "") )
+		menu.itemAtIndex(menu.numberOfItems-1)?.tag = menuTags.ABOUT.rawValue
+		
+		menu.addItem( NSMenuItem.separatorItem() )
+		menu.addItem( NSMenuItem(title: "Quit SuperCapsLock", action: #selector(AppDelegate.quit(_:)), keyEquivalent: "") )
+		menu.itemAtIndex(menu.numberOfItems-1)?.tag = menuTags.QUIT.rawValue
+		statusItem.menu = menu
+		
+		// turn on light if caps lock is already on
+		if(isCapsLockOn()) {
+			activateCapsLock()
+		} else {
+			deactivateCapsLock()
+		}
+		
+		// set up listeners for local and global keyboard events
+		NSEvent.addLocalMonitorForEventsMatchingMask(  NSEventMask.FlagsChangedMask, handler: capsLockOnEventLocal )
+		NSEvent.addGlobalMonitorForEventsMatchingMask( NSEventMask.FlagsChangedMask, handler: capsLockOnEventGlobal )
 	}
 
+	func openAboutWindow(sender: AnyObject) {
+		print("Open About window")
+		window.setIsVisible(true)
+		NSApp.activateIgnoringOtherApps(true)
+	}
+	
+	func quit(sender: AnyObject) {
+		print("Quit application")
+		NSApp.terminate(self)
+	}
+
+	func isCapsLockOn() -> Bool {
+		let eventModifier: UInt32 = GetCurrentKeyModifiers()
+		return eventModifier == 1024
+	}
+	
+	func activateCapsLock() {
+		button.highlighted = true
+		menu.itemWithTag(menuTags.SUPERCAPSLOCK.rawValue)?.setTitleWithMnemonic("CAPS LOCK ON&")
+		keyboard_light.set(UInt64(0xfff))
+	}
+	
+	func deactivateCapsLock() {
+		button.highlighted = false
+		menu.itemWithTag(menuTags.SUPERCAPSLOCK.rawValue)?.setTitleWithMnemonic("Caps lock off&")
+		keyboard_light.set(0)
+	}
+
+	func capsLockOnEventLocal(event: NSEvent)->NSEvent {
+		// is the keyboard event a Caps Lock event (Caps Lock is keyCode 57)
+		if(event.keyCode == UInt16(kVK_CapsLock)) {
+			// check if Caps Lock is turned on
+			if event.modifierFlags.contains(.AlphaShiftKeyMask)	{ activateCapsLock() }
+			else							{ deactivateCapsLock() }
+		}
+		return event
+	}
+	
+	func capsLockOnEventGlobal(event: NSEvent) {
+		// is the keyboard event a Caps Lock event (Caps Lock is keyCode 57)
+		if(event.keyCode == UInt16(kVK_CapsLock)) {
+			// check if Caps Lock is turned on
+			if event.modifierFlags.contains(.AlphaShiftKeyMask)	{ activateCapsLock() }
+			else							{ deactivateCapsLock() }
+		}
+		
+	}
+
+	func acquirePrivileges() -> Bool {
+		// check if we have accessability privileges
+		let accessEnabled = AXIsProcessTrustedWithOptions( [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true] )
+		return accessEnabled == true
+	}
+	
 	func applicationWillTerminate(aNotification: NSNotification) {
 		// Insert code here to tear down your application
 	}
 
-
 }
-
---
+++ CapsLockLight/KeyboardBacklight.swift
@@ -0,0 +1,80 @@
+//
+//  KeyboardBacklight.swift
+//
+// Sidney San Martín
+// Sidnicious
+// https://github.com/Sidnicious
+// https://github.com/CoordinatedHackers
+// https://github.com/CoordinatedHackers/keyboardparty
+
+
+import Foundation
+import IOKit
+
+enum KeyboardBacklightCommand: UInt32, RawRepresentable {
+	case GetSensorReading
+	case GetLEDBrightness
+	case SetLEDBrightness
+	case SetLEDFade
+}
+
+class KeyboardBacklight {
+	
+	// let connect: mach_port_t
+	var connect: mach_port_t
+	
+	init() {
+		let serviceObject = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("AppleLMUController"))
+		assert(serviceObject != 0, "Failed to get service object")
+		
+		connect = 0
+		let status = IOServiceOpen(serviceObject, mach_task_self_, 0, &connect)
+		assert(status == KERN_SUCCESS, "Failed to open IO service")
+	}
+	
+	func get() -> UInt64 {
+		let input: [UInt64] = [0]
+		var output: UInt64 = 0
+		var outputCount: UInt32 = 1
+		
+		let status = IOConnectCallMethod(
+			connect, KeyboardBacklightCommand.GetLEDBrightness.rawValue,
+			input, UInt32(1),
+			nil, 0,
+			&output, &outputCount,
+			nil, nil
+		)
+		assert(status == KERN_SUCCESS, "Failed to set brightness")
+		return output
+	}
+	
+	func set(brightness: UInt64) {
+		let input: [UInt64] = [0, brightness]
+		var output: UInt64 = 0
+		var outputCount: UInt32 = 1
+		
+		let status = IOConnectCallMethod(
+			connect, KeyboardBacklightCommand.SetLEDBrightness.rawValue,
+			input, UInt32(input.count),
+			nil, 0,
+			&output, &outputCount,
+			nil, nil
+		)
+		assert(status == KERN_SUCCESS, "Failed to set brightness")
+	}
+	
+	func fade(brightness: UInt64, duration: UInt64) {
+		let input: [UInt64] = [0, brightness, duration]
+		var output: UInt64 = 0
+		var outputCount: UInt32 = 1
+		
+		let status = IOConnectCallMethod(
+			connect, KeyboardBacklightCommand.SetLEDFade.rawValue,
+			input, UInt32(input.count),
+			nil, 0,
+			&output, &outputCount,
+			nil, nil
+		)
+		assert(status == KERN_SUCCESS, "Failed to set fade")
+	}
+}
