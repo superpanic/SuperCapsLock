266d8eafdfa2c7d143063300f0e5ce268d7cd2e9
superpanic <superpanic@gmail.com>
2016-09-11 22:29:46 +0200

    Settings and user defaults are almost done..

--- CapsLockLight/AppDelegate.swift
+++ CapsLockLight/AppDelegate.swift
@@ -23,10 +23,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	@IBOutlet weak var shiftIsActiveSettingsCheckBox: NSButtonCell!
 	
 	
-	// the status menu item
+		// the status menu item
 	let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
 	
-	// tags used for the menu items
+		// tags used for the menu items
 	enum menuTags: Int {
 		case SUPERCAPSLOCK = 1
 		case ABOUT
@@ -34,59 +34,92 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		case SETTINGS
 	}
 	
-	// the menu icon
+		// the menu icon
 	var button: NSStatusBarButton!
 	
-	// keyboard light controller
+		// keyboard light controller
 	let keyboard_light = KeyboardBacklight()
 	
 	var highLightVal: Int = 0xfff
 	var lowLightVal: Int = 0x0
 	var shiftIsActive: Bool = false
+
+		// read user settings
+	let defaults = NSUserDefaults.standardUserDefaults()
 	
 	func applicationDidFinishLaunching(aNotification: NSNotification) {
 		
-		// user needs to set accessability privileges for this app
+			// user needs to set accessability privileges for this app
 		acquirePrivileges()
 		
 		
-		// read user settings
-		let defaults = NSUserDefaults.standardUserDefaults()
 		
-		// read settings for high and low light
-		var defInt : Int?
 		
-		defInt = defaults.integerForKey("lowLightVal")
+		
+			/* user defaults */
+		
+		print("User defaults: \(Array(defaults.dictionaryRepresentation().keys).count)")
+		
+			// remove all user defaults
+		// let appDomain = NSBundle.mainBundle().bundleIdentifier!
+		// defaults.removePersistentDomainForName(appDomain)
+
+		print("User defaults: \(Array(defaults.dictionaryRepresentation().keys).count)")
+		
+			// read settings for high and low light
+		var defInt : AnyObject?
+		defInt = defaults.objectForKey("lowLightVal")
+		
+		//	let aoInt: AnyObject = Int(1) as NSNumber
+		
+		print("defInt: \(defInt?.integerValue)")
 		if (defInt != nil) {
-			lowLightVal = defInt!
-			
+			lowLightVal = defInt!.integerValue
+			lowLightSettingsSlider.integerValue = lowLightVal
 		}
 		
-		defInt = defaults.integerForKey("highLoghtVal")
-		if (defInt != nil) { highLightVal = defInt! }
-		
+		defInt = defaults.objectForKey("highLightVal")
 
-		// read settings for active shift key
-		var defBool : Bool?
+		print("defInt: \(defInt?.integerValue)")
+		if (defInt != nil) {
+			highLightVal = defInt!.integerValue
+			highLightSettingsSlider.integerValue = highLightVal
+		}
 
-		defBool = defaults.boolForKey("shiftIsActive")
-		if (defBool != nil) { shiftIsActive = defBool! }
-		
-		
+			// read settings for active shift key
+		var defBool : AnyObject?
+		defBool = defaults.objectForKey("shiftIsActive")
+		// defBool = nil
 
 		
-		// setup the menu icon
+		print("shiftIsActive: \(defBool?.boolValue)")
+		if (defBool != nil) {
+			shiftIsActive = defBool!.boolValue
+			if(shiftIsActive) {
+				shiftIsActiveSettingsCheckBox.state = NSOnState
+			} else {
+				shiftIsActiveSettingsCheckBox.state = NSOffState
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+			// setup the menu icon
 		button = statusItem.button
 		button.image = NSImage(named: "StatusItemIconBlk")
 		
-		// set the status menu
+			// set the status menu
 		statusItem.menu = menu
 		
-		// turn on light if caps lock is already on
+			// turn on light if caps lock is already on
 		if( isCapsLockOn() )	{ activateCapsLock() }
 		else			{ deactivateCapsLock() }
 		
-		// set up listeners for local and global keyboard events
+			// set up listeners for local and global keyboard events
 		NSEvent.addLocalMonitorForEventsMatchingMask(  NSEventMask.FlagsChangedMask, handler: capsLockOnEventLocal )
 		NSEvent.addGlobalMonitorForEventsMatchingMask( NSEventMask.FlagsChangedMask, handler: capsLockOnEventGlobal )
 	}
@@ -110,10 +143,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	
 	@IBAction func lowLightSettingsSliderAdjusted(sender: AnyObject) {
 		print("Low light settings slider adjusted")
+		let lowLightValNum: AnyObject = Int(lowLightSettingsSlider.integerValue) as NSNumber
+		defaults.setObject(lowLightValNum, forKey: "lowLightVal")
+		lowLightVal = lowLightValNum.integerValue
+		if( isCapsLockOn() == false ) { deactivateCapsLock() }
 	}
 	
 	@IBAction func highLightSettingsSliderAdjusted(sender: AnyObject) {
 		print("High light settings slider adjusted")
+		let highLightValNum: AnyObject = Int(lowLightSettingsSlider.integerValue) as NSNumber
+		defaults.setObject(highLightValNum, forKey: "highLightVal")
+		highLightVal = highLightValNum.integerValue
+		if( isCapsLockOn() == true ) { activateCapsLock() }
 	}
 	
 	@IBAction func shiftIsActiveSettingsClicked(sender: AnyObject) {
@@ -128,17 +169,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	func activateCapsLock() {
 		button.highlighted = true
 		menu.itemWithTag(menuTags.SUPERCAPSLOCK.rawValue)?.setTitleWithMnemonic("CAPS LOCK ON&")
+		print("highLightval: \(highLightVal)")
 		keyboard_light.set(UInt64(highLightVal))
 	}
 	
 	func deactivateCapsLock() {
 		button.highlighted = false
 		menu.itemWithTag(menuTags.SUPERCAPSLOCK.rawValue)?.setTitleWithMnemonic("Caps lock off&")
+		print("lowLightVal: \(lowLightVal)")
 		keyboard_light.set(UInt64(lowLightVal))
 	}
 
 	func capsLockOnEventLocal(event: NSEvent)->NSEvent {
-		// is the keyboard event a Caps Lock event (Caps Lock is keyCode 57)
+			// is the keyboard event a Caps Lock event (Caps Lock is keyCode 57)
 		if(event.keyCode == UInt16(kVK_CapsLock)) {
 			// check if Caps Lock is turned on
 			if event.modifierFlags.contains(.AlphaShiftKeyMask)	{ activateCapsLock() }
@@ -148,7 +191,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	}
 	
 	func capsLockOnEventGlobal(event: NSEvent) {
-		// is the keyboard event a Caps Lock event (Caps Lock is keyCode 57)
+			// is the keyboard event a Caps Lock event (Caps Lock is keyCode 57)
 		if(event.keyCode == UInt16(kVK_CapsLock)) {
 			// check if Caps Lock is turned on
 			if event.modifierFlags.contains(.AlphaShiftKeyMask)	{ activateCapsLock() }
@@ -158,7 +201,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	}
 
 	func acquirePrivileges() -> Bool {
-		// check if we have accessability privileges
+			// check if we have accessability privileges
 		let accessEnabled = AXIsProcessTrustedWithOptions( [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true] )
 		return accessEnabled == true
 	}
